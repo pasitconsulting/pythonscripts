2777001a3575faae4e9b78935948feb7372c7e8c
root <root@tomcat.example.com>
2016-05-16 18:47:27 +0100

    tidy up

--- abstractedAPI/postgresbackup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#dump contents of postgres databases
-/usr/bin/pg_dumpall > /opt/postgresqlbkdumps/postgresbackup`date +\%d%m%y_%M%S`
-
-#upload backup to skyscapestorage
-/opt/scripts/api-filecreate.sh
